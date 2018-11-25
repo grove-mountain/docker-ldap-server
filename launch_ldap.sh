dcb7c8f81df4c56f8ca4ad5d6d2852a6994fc335
Jake Lundberg <grove.mountain.github@nym.hush.com>
2018-11-25 00:40:32 -0800

    master: adding in read-only user

--- launch_ldap.sh
+++ launch_ldap.sh
@@ -3,6 +3,9 @@ LDAP_ORGANISATION="HashiCorp Inc"
 LDAP_DOMAIN="hashidemos.com"
 LDAP_ADMIN_PASSWORD="hashifolk"
 LDAP_HOSTNAME="ldap.hashidemos.com"
+LDAP_READONLY_USER=true
+LDAP_READONLY_USER_USERNAME=read-only
+LDAP_READONLY_USER_PASSWORD="devsecopsFTW"
 
 # Get the relative directory name
 # Sorry windows folks, you're gonna have to figure this out and modify it for you pleasure
@@ -15,6 +18,9 @@ docker run --hostname ${LDAP_HOSTNAME} \
   -e LDAP_ORGANISATION="${LDAP_ORGANISATION}" \
   -e LDAP_DOMAIN="${LDAP_DOMAIN}" \
   -e LDAP_ADMIN_PASSWORD="${LDAP_ADMIN_PASSWORD}" \
+  -e LDAP_READONLY_USER=${LDAP_READONLY_USER} \
+  -e LDAP_READONLY_USER_USERNAME=${LDAP_READONLY_USER_USERNAME} \
+  -e LDAP_READONLY_USER_PASSWORD=${LDAP_READONLY_USER_PASSWORD} \
   -v ${DIR}/ldif:/container/service/slapd/assets/config/bootstrap/ldif/custom \
   --name openldap \
-  --detach osixia/openldap:${DOCKER_VERSION} --copy-service --loglevel debug
+  --detach osixia/openldap:${DOCKER_VERSION} --copy-service
